5317b8be5a2d316fe768cf904cbb2b57360fb7e9
Buğra Kurnaz <muammerkurnaz1071@gmail.com>
2023-02-22 01:36:39 +0300

    Url tags and templates

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 # Create your views here.
 
 monthly_challenges = {
@@ -20,13 +21,10 @@ monthly_challenges = {
 
 def index(request):
     months = list(monthly_challenges.keys())
-    response_data = ""
-    for month in months:
-        capitalized_month = month.capitalize()
-        desired_path = reverse("month-challenge", args = [month])
-        response_data += f"<li><a href=\"{desired_path}\"> {capitalized_month}</a> </li>"
-    html_data = f"<ul>{response_data}</ul>"
-    return HttpResponse(html_data)
+ 
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
 
 def monthly_challenge_as_number(request, month):
     months = list(monthly_challenges.keys())
@@ -35,13 +33,16 @@ def monthly_challenge_as_number(request, month):
 
     desired_month = months[month-1]
     desired_path = reverse("month-challenge", args = [desired_month])
-    return HttpResponseRedirect(desired_path)
+    return HttpResponseRedirect(desired_path) 
 
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        html_data = f"<h1>{challenge_text}</h1>"
-        return HttpResponse(html_data)
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "month": month
+        })
+        
     except:
         return HttpResponseNotFound("This page is not supported!")
     
